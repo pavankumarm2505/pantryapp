dd8e5ee62d9a2e26b7d141bd48f3b33b4be7fb45
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-07-30 14:47:44 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import { Box, Stack } from "@mui/material"
+const item = [
+  "tomato",
+  "potato",
+  "onion",
+  "carrot",
+  "pepper",
+  "garlic",
+]
+
+export default function Home() {
+  return(
+  <Box
+    width="100vw" height="100vh"
+    display={"flex"}
+    justifyContent={"center"}
+    alignItems={"center"}
+
+  >
+    <Stack width="800px" height="600px" spacing={2}>
+      {item.map((i) => {
+        <Box key={i} width = "100%" height="100px" display={"flex"} justifyContent={"center"} alignItems={"center"} bgcolor={'#f0f0f0'}>{i}</Box>
+      })}
+    </Stack>
+  </Box>
+  )
+}
